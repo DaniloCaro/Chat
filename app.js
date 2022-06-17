140001df0acb628caeb15750d3cf777f45143199
DaniloCaro <94326335+DaniloCaro@users.noreply.github.com>
2022-06-17 14:50:33 -0500

    Correccion de port

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.get('/', (req, res) => {
 })
 
 let port = process.env.PORT || 8080;
-server.listen(8080), () => {
+server.listen(port), () => {
     console.log('Server is running on port 3000')
 }
